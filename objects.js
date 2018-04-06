fee6532bb3dc4abfb40cd76a40ec31d809021c5a
Anna Dybas <a.m.dybas@gmail.com>
2018-04-06 19:54:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist =  {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songName;
+  return playlist[artistName] = songName;
 }
 
 function removeFromPlaylist(playlist, artistName) {
